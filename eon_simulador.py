b2bc03ffce55c07f558f112456a62fbb05d3846a
dilwar-crnlab <csp21105@tezu.ac.in>
2024-08-11 02:19:16 +0530

    Pushing all files to git

--- eon_simulador.py
+++ eon_simulador.py
@@ -37,29 +37,6 @@ class Simulador(object):
 				if i!= j:
 					self.k_paths[i,j] = self.k_shortest_paths(topology, i, j, N_PATH, weight='weight')
 
-		for count in range(1, NUM_OF_REQUESTS + 1):
-			yield self.env.timeout(self.random.expovariate(rate))
-			class_type = np.random.choice(CLASS_TYPE, p=CLASS_WEIGHT)
-			src, dst = self.random.sample(self.nodes, 2)
-			bandwidth = self.random.choice(BANDWIDTH)
-			holding_time = self.random.expovariate(HOLDING_TIME)
-			paths = self.k_paths[src,dst]
-			flag = 0
-			for i in range(N_PATH):
-				distance = int(self.Distance(paths[i]))
-				num_slots = int(math.ceil(self.Modulation(distance, bandwidth)))
-				self.check_path = self.PathIsAble(num_slots,paths[i])
-				if self.check_path[0] == True:
-					self.cont_req += 1
-					self.FirstFit(count, self.check_path[1],self.check_path[2],paths[i])
-					spectro = [self.check_path[1], self.check_path[2]]
-					desalocate = Desalocate(self.env)
-					self.env.process(desalocate.Run(count,paths[i],spectro,holding_time))
-					flag = 1
-					break 
-			if flag == 0:
-					self.NumReqBlocked +=1
-
 	# Calculates the path distance according to the edge weights            
 	def Distance(self, path):
 		global topology 
@@ -83,42 +60,114 @@ class Simulador(object):
 		else:
 			return (float(demand) / float(1 * SLOT_SIZE))
 
-	##Perform spectrum allocation using First-fit
-	def FirstFit(self,count,i,j,path):
-		global topology
-		inicio = i 
-		fim =j
-		for i in range(0,len(path)-1):
-			for slot in range(inicio,fim):
-				#print slot
-				topology[path[i]][path[i+1]]['capacity'][slot] = count
-			topology[path[i]][path[i+1]]['capacity'][fim] = 'GB'
 
-	## Checks whether the chosen path has available spectrum for the requested demand
-	def PathIsAble(self, nslots, path):
-		global topology
-		cont = 0
-		t = 0
-		for slot in range (0,len(topology[path[0]][path[1]]['capacity'])):
-			if topology[path[0]][path[1]]['capacity'][slot] == 0:
-				k = 0
-				for ind in range(0,len(path)-1):
-					if topology[path[ind]][path[ind+1]]['capacity'][slot] == 0:
-						k += 1
-				if k == len(path)-1:
-					cont += 1
-					if cont == 1:
-						i = slot
-					if cont > nslots:
-						j = slot
-						return [True,i,j]
-					if slot == len(topology[path[0]][path[1]]['capacity'])-1:
-							return [False,0,0]
-				else:
-					cont = 0
-					if slot == len(topology[path[0]][path[1]]['capacity'])-1:
-						return [False,0,0]
-			else:
-				cont = 0
-				if slot == len(topology[path[0]][path[1]]['capacity'])-1:
-					return [False,0,0]
+class QLearningRouter(Simulador):
+    def __init__(self, env):
+        super().__init__(env)
+        self.q_table = {}
+        self.initialize_q_table()
+
+    def initialize_q_table(self):
+        for node in self.nodes:
+            self.q_table[node] = {neighbor: 0 for neighbor in topology[node]}
+
+    def get_action(self, state, possible_actions):
+        if random() < EPSILON:
+            return choice(possible_actions)
+        else:
+            return max(possible_actions, key=lambda a: self.q_table[state][a])
+
+    def update_q_value(self, state, action, reward, next_state):
+        current_q = self.q_table[state][action]
+        max_next_q = max(self.q_table[next_state].values())
+        new_q = (1 - ALPHA) * current_q + ALPHA * (reward + GAMMA * max_next_q)
+        self.q_table[state][action] = new_q
+
+    def find_k_paths(self, source, destination):
+        paths = []
+        for _ in range(Q_LEARNING_EPISODES):
+            path = [source]
+            current_node = source
+            visited = set([source])
+            while current_node != destination:
+                possible_actions = [node for node in topology[current_node] if node not in visited]
+                if not possible_actions:
+                    break  # No valid moves, abandon this path
+                action = self.get_action(current_node, possible_actions)
+                reward = 1 / topology[current_node][action]['weight']
+                self.update_q_value(current_node, action, reward, action)
+                path.append(action)
+                visited.add(action)
+                current_node = action
+            if current_node == destination:
+                paths.append(tuple(path))
+        return sorted(set(paths), key=len)[:K_PATHS]
+
+class PLIAwareRMSA(QLearningRouter):
+    def __init__(self, env):
+        super().__init__(env)
+
+    def calculate_fragmentation(self, path):
+        total_free_slots = 0
+        total_free_blocks = 0
+        current_block = 0
+        for i in range(len(path) - 1):
+            link_capacity = topology[path[i]][path[i+1]]['capacity']
+            for slot in link_capacity:
+                if slot == 0:
+                    total_free_slots += 1
+                    current_block += 1
+                else:
+                    if current_block > 0:
+                        total_free_blocks += 1
+                        current_block = 0
+            if current_block > 0:
+                total_free_blocks += 1
+        if total_free_slots == 0:
+            return 1  # Fully fragmented
+        return 1 - (total_free_blocks / total_free_slots)
+
+    def find_feasible_spectrum(self, path, num_slots):
+        for i in range(SLOTS - num_slots + 1):
+            if all(topology[path[j]][path[j+1]]['capacity'][i:i+num_slots] == [0]*num_slots 
+                   for j in range(len(path)-1)):
+                return i, i + num_slots - 1
+        return None
+
+    def Run(self, rate):
+        for count in range(1, NUM_OF_REQUESTS + 1):
+            yield self.env.timeout(self.random.expovariate(rate))
+            src, dst = self.random.sample(self.nodes, 2)
+            bandwidth = self.random.choice(BANDWIDTH)
+            holding_time = self.random.expovariate(HOLDING_TIME)
+            print("Request", count, "(",src, dst, bandwidth, holding_time,")" )
+
+            paths = self.find_k_paths(src, dst)
+            print("paths",paths)
+            for path in paths:
+                distance = int(self.Distance(path))
+                num_slots = int(math.ceil(self.Modulation(distance, bandwidth)))
+                spectrum = self.find_feasible_spectrum(path, num_slots)
+
+                if spectrum:
+                    self.allocate_path(path, spectrum, count)
+                    fragmentation = self.calculate_fragmentation(path)
+                    reward = 1 - fragmentation  # Higher reward for less fragmentation
+                    self.update_q_values(path, reward)
+                    desalocate = Desalocate(self.env)
+                    self.env.process(desalocate.Run(count, path, spectrum, holding_time))
+                    print("Request", count, "established")
+                    break
+					
+            else:
+                self.NumReqBlocked += 1
+                print("Request", count, "blocked")
+
+    def allocate_path(self, path, spectrum, count):
+        for i in range(len(path) - 1):
+            for slot in range(spectrum[0], spectrum[1] + 1):
+                topology[path[i]][path[i+1]]['capacity'][slot] = count
+
+    def update_q_values(self, path, reward):
+        for i in range(len(path) - 1):
+            self.update_q_value(path[i], path[i+1], reward, path[i+1])
--- run.py
+++ run.py
@@ -1,24 +1,43 @@
-from eon_simulador import Simulador
+import matplotlib.pyplot as plt
+from eon_simulador import PLIAwareRMSA
 import simpy
-from random import *
+from random import seed
 from config import *
 import numpy as np
 
 def main(args):
-	for e in range(ERLANG_MIN, ERLANG_MAX+1, ERLANG_INC):
-		print("-----------------------------")
-		for rep in range(10):
-			rate = e / HOLDING_TIME
-			seed(RANDOM_SEED[rep])
-			env = simpy.Environment()
-			simulador = Simulador(env)
-			env.process(simulador.Run(rate))
-			env.run()
-			print("Erlang", e, "Simulation...", rep)
-			print("#Total Request", NUM_OF_REQUESTS, "Blocked", simulador.NumReqBlocked, "B_%", (simulador.NumReqBlocked/NUM_OF_REQUESTS)*100)
-			
-	return 0
+    blocking_ratios = {}
+
+    for e in range(ERLANG_MIN, ERLANG_MAX+1, ERLANG_INC):
+        print("-----------------------------")
+        blocking_ratios[e] = []
+        for rep in range(REP):
+            rate = e / HOLDING_TIME
+            seed(RANDOM_SEED[rep])
+            env = simpy.Environment()
+            simulador = PLIAwareRMSA(env)
+            env.process(simulador.Run(rate))
+            env.run()
+            blocking_ratio = (simulador.NumReqBlocked / NUM_OF_REQUESTS) * 100
+            blocking_ratios[e].append(blocking_ratio)
+            print(f"Erlang {e}, Simulation... {rep}")
+            print(f"#Total Request {NUM_OF_REQUESTS}, Blocked {simulador.NumReqBlocked}, B_% {blocking_ratio:.2f}")
+
+    # Calculate average blocking ratio for each Erlang value
+    avg_blocking_ratios = {e: np.mean(ratios) for e, ratios in blocking_ratios.items()}
+
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    plt.plot(list(avg_blocking_ratios.keys()), list(avg_blocking_ratios.values()), marker='o')
+    plt.xlabel('Erlang')
+    plt.ylabel('Blocking Ratio (%)')
+    plt.title('Average Blocking Ratio vs Erlang')
+    plt.grid(True)
+    plt.savefig('blocking_ratio_vs_erlang.png')
+    plt.show()
+
+    return 0
 
 if __name__ == '__main__':
-	import sys
-	sys.exit(main(sys.argv))
+    import sys
+    sys.exit(main(sys.argv))
